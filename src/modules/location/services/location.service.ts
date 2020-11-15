3897ff69879bd5e48302b57861fe2e81d7c34b70
Eric Zorn <zornwebdev@gmail.com>
2020-11-14 23:15:48 -0500

    fix: location logic

--- src/modules/location/services/location.service.ts
+++ src/modules/location/services/location.service.ts
@@ -1,23 +1,37 @@
 import { StoreSearchLocationInput } from 'src/shared/dto';
-import { HttpService, Injectable } from '@nestjs/common';
+import { HttpService, Injectable, Logger } from '@nestjs/common';
 import { LocationCoords } from '../dto/location.dto';
 
 @Injectable()
 export class LocationService {
+  private readonly logger = new Logger('Location Service');
+
   constructor(private readonly httpService: HttpService) {}
 
+  /**
+   * Obtain the user's coordinates from the provided address and query the
+   * Google Maps API
+   *
+   * @param {StoreSearchLocationInput} address
+   * @return {*}  {Promise<LocationCoords>}
+   * @memberof LocationService
+   */
   public async getCoords(
     address: StoreSearchLocationInput,
   ): Promise<LocationCoords> {
     let parsedAddress = Object.keys(address).reduce((acc, key) => {
-      acc += `${key}=${address[key]}+`;
+      acc += `${address[key]}+`;
       return acc;
     }, '');
 
     // Trim the ending of the address
     if (parsedAddress[parsedAddress.length - 1] === '+') {
-      parsedAddress = parsedAddress.slice(0, parsedAddress.length - 2);
+      parsedAddress = parsedAddress.slice(0, parsedAddress.length - 1);
     }
+
+    this.logger.debug(
+      `Finding location for address ${JSON.stringify(address)}`,
+    );
 
     const { data } = await this.httpService
       .get('/geocode/json', {
